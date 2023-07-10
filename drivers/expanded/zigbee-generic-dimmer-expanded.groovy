ab493272d68737c6b55ac6d345150b638b3d9062
kkossev <kossevkrassimir@gmail.com>
2023-07-10 18:02:40 +0300

    Zigbee - Generic Dimmer (w/ healthStatus) 2023/07/10 6:00 PM

--- drivers/expanded/zigbee-generic-dimmer-expanded.groovy
+++ drivers/expanded/zigbee-generic-dimmer-expanded.groovy
@@ -23,7 +23,7 @@
  */
 
 def version() { "2.0.0" } 
-def timeStamp() {"2023/07/10 10:37 AM"}
+def timeStamp() {"2023/07/10 6:00 PM"}
 
 // BEGIN:getDefaultImports()
 import groovy.json.JsonSlurper
@@ -57,6 +57,7 @@ metadata {
         attribute "lastCheckinEpoch", "number"
         attribute "notPresentCounter", "number"
         attribute "restoredCounter", "number"
+        attribute 'healthStatus', 'enum', ['unknown', 'offline', 'online']        
         // END:  getMetadataAttributesForLastCheckin()
 
         // BEGIN:getCommandsForPresence()
